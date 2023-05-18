2b12df338da3ab650d87bcc348b8c7bde10505f4
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-18 17:01:39 -0300

    populando a tela

--- scripts/FetchCartoons.js
+++ scripts/FetchCartoons.js
@@ -2,5 +2,15 @@ var urlCartoon = 'https://api.sampleapis.com/cartoons/cartoons2D'
 
 fetch(urlCartoon)
     .then(res=>res.json())
-    .then(data=>console.log(data))
+    .then(data=>PopularATela(data))
     .catch(erro =>console.log(erro))
+
+function PopularATela(data){
+    const div = document.getElementById('cartoonsList');
+    const allCards = data.map(item=>`
+        <div>
+            <h2>${item.title}</h2>
+        </div>
+    `).join('');
+    div.innerHTML = allCards;
+}
