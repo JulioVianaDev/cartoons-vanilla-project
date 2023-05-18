14ae285ec8c63061bd9cd3f6d17f19b4166b4b44
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-18 19:29:32 -0300

    salvando no cache

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,16 +1,18 @@
 const navbar = document.getElementById("navbar")
+const checkNavName = localStorage.getItem("userName")
 navbar.innerHTML= ` 
     <div style="display: inline-block;">
     <a class="links-navbar" href="./index.html">Home</a>
     <a class="links-navbar" href="./cartoons.html">Cartoons</a>
     </div>
     <div id="info-navbar">
-        <div id="nome-navbar"></div>
+        <div id="nome-navbar"><p id="nome-na-nav">${checkNavName}</p></div>
         <img onclick="logar()" src="./images/navbar/logo.jpeg" id="navbar-image">
     </div>
 `
 function logar(){
     var userName = prompt('Please enter your name:');
+    localStorage.setItem("userName",userName)
     const userNameCampo = document.getElementById('nome-navbar');
     userNameCampo.innerHTML =  `<p id="nome-na-nav">${userName}</p>`
     if (userName === null){
