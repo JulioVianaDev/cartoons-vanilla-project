c25e731bae7311c800fdd23a118217e3ae5d4ab4
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-18 16:32:54 -0300

    pegando os dados

---
+++ scripts/navbar.js
@@ -0,0 +1,8 @@
+const navbar = document.getElementById("navbar")
+navbar.innerHTML= ` 
+    <div style="display: inline-block;">
+    <a class="links-navbar" href="./index.html">Home</a>
+    <a class="links-navbar" href="./cartoons.html">Cartoons</a>
+    </div>
+    <img src="./images/navbar/logo.jpeg" id="navbar-image">
+`
