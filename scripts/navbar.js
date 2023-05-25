727d27e590566e716208ebe0a96e8e62e2011c8b
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-25 16:33:53 -0300

    local storage para cookies

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,5 +1,5 @@
 const navbar = document.getElementById("navbar")
-const checkNavName = localStorage.getItem("userName")
+const checkNavName = localStorage.getItem('nome')
 navbar.innerHTML= ` 
     <div style="display: inline-block;">
     <a class="links-navbar" href="./index.html">Home</a>
@@ -7,15 +7,16 @@ navbar.innerHTML= `
     </div>
     <div id="info-navbar">
         <div id="nome-navbar"><p id="nome-na-nav">${checkNavName}</p></div>
-        <img onclick="logar()" src="./images/navbar/logo.jpeg" id="navbar-image">
+        <img onclick ="logar()" src="./images/navbar/logo.jpeg" id="navbar-image">
     </div>
 `
+
 function logar(){
-    var userName = prompt('Please enter your name:');
-    localStorage.setItem("userName",userName)
+    var userName = prompt('Por favor coloque seu nome: ');
+    localStorage.setItem('nome',userName)
     const userNameCampo = document.getElementById('nome-navbar');
-    userNameCampo.innerHTML =  `<p id="nome-na-nav">${userName}</p>`
-    if (userName === null){
-        userNameCampo.innerHTML =  `<p id="nome-na-nav"></p>`
+    userNameCampo.innerHTML = `<p  id="nome-na-nav">${userName} </p>`;
+    if(userName === null){
+        userNameCampo.innerHTML = `<p  id="nome-na-nav"></p>`;
     }
 }
