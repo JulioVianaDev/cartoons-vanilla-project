16eadc054f80f4eea71fe0714d84386d32254b8a
JulioVianaDev <JulioVianaDev@gmail.com>
2023-06-01 16:58:21 -0300

    sendMessage criado com javascript

--- scripts/zap.js
+++ scripts/zap.js
@@ -1,3 +1,5 @@
+zap = document.getElementById('zap')
+zap.innerHTML = '<img id="sendMessageBtn" src="https://seeklogo.com/images/W/whatsapp-icon-logo-8CA4FB831E-seeklogo.com.png" alt="">';
 document.getElementById("sendMessageBtn").addEventListener("click",()=>SendMessage());
 
 const SendMessage  = ()=>{
